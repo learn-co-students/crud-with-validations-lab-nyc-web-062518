95f45ac72f3540ef17b7d1db12246d94e1c571c1
Jack Yeung <jyeung.se@gmail.com>
2018-07-26 23:50:58 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,19 @@
+class Song < ActiveRecord::Base
+
+  validates :title, :artist_name, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validates :title, uniqueness: { scope: [:artist_name, :release_year], message: "cannot be repeated by the same artist in the same year"}
+
+
+  with_options if: :released? do |song|
+  song.validates :release_year, presence: true
+  song.validates :release_year, numericality: {
+    less_than_or_equal_to: Date.today.year
+  }
+end
+
+  def released?
+    released
+  end
+
+end
